3e93e0669da0a112130fea0ff04f343aab56d77e
Sven Tennie <sven.tennie@gmail.com>
2021-10-29 15:20:06 +0200

    Introduce 2nd field in SomeExceptionWithLocation

--- compiler/GHC/Utils/Panic.hs
+++ compiler/GHC/Utils/Panic.hs
@@ -109,7 +109,7 @@ data GhcException
   | PprProgramError String SDoc
 
 instance Exception GhcException where
-  fromException (SomeExceptionWithLocation e)
+  fromException (SomeExceptionWithLocation e _)
     | Just ge <- cast e = Just ge
     | Just pge <- cast e = Just $
         case pge of
